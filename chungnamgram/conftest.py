a9e235fc0e519ccdf05d5c9d71a1fe4b9ab1a65d
Hyunkim <zhyunz123@gmial.com>
2022-05-20 00:39:21 +0900

    school

---
+++ chungnamgram/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from chungnamgram.users.models import User
+from chungnamgram.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
